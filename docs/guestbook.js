c0f4071286cb7a5834e5ff88b79ed9e770b80241
Aleema Malik <49048714+AleemaMalik@users.noreply.github.com>
2020-07-15 22:49:57 -0400

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://f971c587.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://e3bb4a78.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
